55725383d05caf6baa35f3babf3f742575fec8f8
maruf <maruf.harsono@gmail.com>
2020-10-09 11:00:48 +0700

    Form: added checkbox component

---
+++ components/global/PkbrCheckbox.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-checkbox v-model="tempValue" :color="color" :label="label"></v-checkbox>
+</template>
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: null
+    },
+    label: {
+      type: String,
+      default: null
+    },
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      tempValue: this.value
+    }
+  },
+  watch: {
+    value(val) {
+      this.tempValue = val
+    },
+    tempValue(val) {
+      this.$emit('input', val)
+    }
+  }
+}
+</script>
